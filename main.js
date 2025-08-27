d9bc7ec40270aa1c7a0ecb7e92d43c87a300ed5e
Matheus <matheusleao585@gmail.com>
2025-08-27 15:50:49 -0300

    feat(main.js): Submenu

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 //console.log("processo principal")
-const { app, BrowserWindow, nativeTheme } = require('electron')
+const { app, BrowserWindow, nativeTheme, Menu, shell } = require('electron')
 
+//Janela principal
 const createWindow = () => {
     nativeTheme.themeSource = 'system'
     const win = new BrowserWindow({
@@ -9,7 +10,21 @@ const createWindow = () => {
         icon: './src/public/img/ludus-primal.png'
     })
 
+    //menu personalido
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template))
+
     win.loadFile('./src/views/index.html')
+}
+
+//janela sobre
+const aboutWindow = () => {
+    const about = new BrowserWindow({
+        width: 360,
+        height: 220,
+        icon: './src/public/img/ludus-primal.png'
+    })
+
+    about.loadFile('./src/views/sobre.html')
 }
 
 app.whenReady().then(() => {
@@ -23,3 +38,61 @@ app.whenReady().then(() => {
 app.on('window-all-closed', () => {
     if(process.platform !== 'darwin') app.quit()
 })
+
+//template menu
+const template = [
+    {
+        label: 'Arquivo',
+        submenu: [
+            {
+                label: 'Sair',
+                click: () => app.quit(),
+                accelerator: 'Alt+F4'
+            }
+        ]
+    },
+    {
+        label:'Exibir',
+        submenu: [
+            {
+                label: 'Recarregar',
+                role: 'reload',
+            },
+            {
+                label:'Ferramentas do desenvolvedor',
+                role:'toggleDevTools'
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label:'Aplicar zoom',
+                role:'zoomIn'
+            },
+            {
+                label:'Reduzir zoom',
+                role:'zoomOut'
+            },
+            {
+                label:'Restaurar zoom',
+                role:'resetZoom'
+            }
+        ]
+    },
+    {
+        label:'Ajuda',
+        submenu:[
+            {
+                label:'docs',
+                click: () => shell.openExternal('https://www.electronjs.org/docs/latest')
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label:'Sobre',
+                click: () => aboutWindow()
+            }
+        ]
+    }
+]
